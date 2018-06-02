d46d209abb2757d241af72a3b50fad9510392e54
Lina Ragauskaite <ragauskl@gmail.com>
2018-06-02 20:31:22 +0100

    Completed PDF generation and live release build shortcut

--- pdf-m.js
+++ pdf-m.js
@@ -1,24 +1,60 @@
 const puppeteer = require('puppeteer')
+// const shell = require('shelljs')
+// const httpServer = require('http-server')
+const { spawn } = require('child_process')
+let server, page, browser
 
-;(async () => {
-  const browser = await puppeteer.launch()
-  const page = await browser.newPage()
+const createBrowser = async () => {
+  browser = await puppeteer.launch({headless: true, devtools: false})
+  page = await browser.newPage()
   await page.setViewport({width: 1600, height: 900})
+}
 
-  await page.goto('http://localhost:4200/#/pdf', {waitUntil: ['networkidle2', 'load']})
+const createServer = () => {
+  server = spawn('cmd', ['/c', 'ng', 'serve'])
+}
 
+const connectPage = async () => {
+  return page.goto('http://localhost:4200/#/pdf', {waitUntil: ['networkidle2']})
+}
+
+const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms))
+
+const generatePdf = async () => {
   const height = await page.evaluate(() => {
-    return document.body.scrollHeight
+    return document.body.clientHeight
   })
 
+  server.kill(0)
+
   await page.pdf({
-    path: 'Lina_Ragauskaite.pdf',
+    path: './src/assets/Lina_Ragauskaite.pdf',
     printBackground: true,
     width: 1600,
     height: height,
     pageRanges: '1'
   })
+  console.log('Pdf saved.')
+}
 
+;(async () => {
+  await createServer()
+  await createBrowser()
+
+  let connected = false
+  console.log('Waiting for angular to finish build... (40s)')
+  await delay(40000)
+  while (!connected) {
+    try {
+      await connectPage()
+      connected = true
+      console.log('Page loaded.')
+    } catch (error) {
+      console.log('Error laoding page.', error)
+    }
+  }
+
+  await generatePdf()
   await browser.close()
   process.exit()
 })()
--- pdf.js
+++ pdf.js
@@ -1,32 +1,52 @@
 const puppeteer = require('puppeteer')
-const { spawn } = require('child_process')
+const shell = require('shelljs')
+const fs = require('fs')
+// const { spawn } = require('child_process')
 let server, page, browser
 
+const pdfFilePath = './src/assets/Lina_Ragauskaite.pdf'
+const buildLatestForPdf = () => {
+  shell.exec('npm run build-noref')
+}
+
+const buildLatestForLive = () => {
+  shell.exec('npm run build')
+}
 const createBrowser = async () => {
-  browser = await puppeteer.launch({headless: false, devtools: true})
+  browser = await puppeteer.launch({headless: true, devtools: false})
   page = await browser.newPage()
   await page.setViewport({width: 1600, height: 900})
 }
 
 const createServer = () => {
-  server = spawn('cmd', ['/c', 'ng', 'serve'])
+  // server = spawn('cmd', ['/c', 'http-server', './dist/', '-p', '4200'])
+  server = shell.exec('http-server ./dist/ -p 4200', {async: true})
 }
 
 const connectPage = async () => {
   return page.goto('http://localhost:4200/#/pdf', {waitUntil: ['networkidle2', 'load']})
 }
 
-const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms))
+// const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms))
+
+const pdfValid = () => {
+  const stats = fs.statSync(pdfFilePath)
+  const fileSizeInBytes = stats.size
+  const fileSizeInKb = fileSizeInBytes * 0.001
+  return fileSizeInKb > 100
+}
 
 const generatePdf = async () => {
   const height = await page.evaluate(() => {
-    return document.body.scrollHeight
+    return document.body.clientHeight
   })
 
-  server.kill(0)
+  killNodes(server.pid)
+  server.kill()
+  console.log('Http-server killed.')
 
   await page.pdf({
-    path: './src/assets/Lina_Ragauskaite.pdf',
+    path: pdfFilePath,
     printBackground: true,
     width: 1600,
     height: height,
@@ -34,27 +54,45 @@ const generatePdf = async () => {
   })
 }
 
+const killNodes = (id) => {
+  // if (process.platform === 'win32') {
+  shell.exec(`taskkill /pid ${id} /f /t`)
+  // } else {
+  //   shell.exec('pkill node')
+  // }
+}
+
 ;(async () => {
+  buildLatestForPdf()
+  console.log('Build complete for PDF capture.')
   await createServer()
+  console.log('Http-server started.')
   await createBrowser()
+  console.log('Broswer opened.')
 
   let connected = false
-
   while (!connected) {
     try {
       await connectPage()
       connected = true
-      console.log('connected')
+      console.log('Page loaded.')
     } catch (error) {
-      console.log('F', error)
+      console.log('Error laoding page.', error)
     }
   }
-  await delay(2000)
-  console.log('...')
-  page.on('load', async () => {
-    console.log('!!!')
-    await generatePdf()
-    await browser.close()
-  })
-  process.exit()
+
+  await generatePdf()
+  console.log('PDF generated.')
+  await page.close()
+  await browser.close()
+  console.log('Browser closed.')
+  if (pdfValid()) {
+    console.log('PDF OK')
+    buildLatestForLive()
+    console.log('Build complete for live release.')
+    process.exit(0)
+  } else {
+    console.log('PDF NOK')
+    process.exit(1)
+  }
 })()
